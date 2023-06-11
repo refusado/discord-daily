a1f40312dcd00d5031bd1e029edfdae7f63e2ee1
Renan <orefusado@gmail.com>
2023-06-11 04:51:27 -0300

    improve error handling + small refactor

--- src/main.js
+++ src/main.js
@@ -5,29 +5,56 @@ import tips from './tips.js';
 config();
 
 const hour = getCurrentHour();
-const message = getMessage();
+const text = getMessage();
 const urls = JSON.parse(process.env.WEBHOOK_URLS).urls;
 const body = {
   "embeds": [
     {
-      "title": `\`${hour}\`:bulb: Daily tip time!` ,
-      "description": message + "\n\n[Source](https://github.com/refusado/discord-daily)",
+      "title": `:bulb: Daily tip time!` ,
+      "description": `#${hour} ${text} \n\n[About](https://github.com/refusado/discord-daily)`,
       "color": 16436796
     }
   ]
 }
 
-const requests = urls.map(url => axios.post(url, body));
+const requests = urls.map(async url => {
+  try {
+    const webhook = /https?:\/\/(?:ptb\.|canary\.)?discord\.com\/api(?:\/v\d{1,2})?\/webhooks\/(\d{17,19})\/([\w-]{68})/i;
+    if (!webhook.test(url))
+      throw new Error("Invalid webhook URL.");
 
-try {
-  const responses = await Promise.all(requests);
-  responses.forEach((res, i) => {
-    console.log(`Message sent for ${urls[i]}. Status: ${res.status}`);
-  });
-} catch (err) {
-  console.log(err.message);
-}
+    return await axios.post(url, body);
+  } catch (error) {
+    if (!error.response) {
+      return error
+    }
+    error.response.failed = true;
 
+    return error.response;
+  }
+});
+
+const responses = await Promise.all(requests);
+responses.forEach((res, i) => {
+  if (res instanceof Error) {
+    let message = `Request failed for ${urls[i]}`;
+        message += `\n Message: ${res.message}`;
+
+    console.log(message);
+  } else if (res.failed) {
+    let message = `Request failed for ${urls[i]}`;
+        message += `\n Status: ${res.status ?? 0}`;
+        message += `\n Message: ${res.data.message ?? ""}`;
+        message += `\n Code: ${res.data.code ?? 0}`;
+
+    console.log(message);
+  } else {
+    let message = `Request sent for ${urls[i]}`;
+        message += `\n Status: ${res.status}`;
+
+    console.log(message);
+  }
+});
 
 
 function getMessage() {
