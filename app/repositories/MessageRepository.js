1bfc9f3d6e2d8f1db3473242b1e0e6e2a80acba2
Renan <orefusado@gmail.com>
2023-06-25 03:38:33 -0300

    message repository

---
+++ app/repositories/MessageRepository.js
@@ -0,0 +1,102 @@
+import { writeFile } from 'fs';
+import { messages } from '../../data/messages.js';
+
+class MessageRepository {
+  async insert(messageData) {
+    try {
+      return new Promise(async resolve => {
+        const id = messages[messages.length - 1].id + 1;
+        const content = { id, ...messageData };
+        const size = 1;
+
+        const updatedMessages = [...messages, content];
+        await saveData(updatedMessages);
+    
+        resolve({ size, content });
+      });
+    } catch (error) {
+      console.error('Error while inserting message:', error);
+      throw new Error('Failed to insert message.');
+    }
+  }
+
+  async getAll() {
+    return new Promise(resolve => {
+      const content = messages;
+      const size = messages.length;
+
+      resolve({ size, content });
+    });
+  }
+
+  async find(message) {
+    return new Promise(resolve => {
+      const content = messages.filter(({ text }) => text == message);
+      const size = content.length;
+
+      resolve({ size, content });
+    });
+  }
+
+  async findById(id) {
+    return new Promise(resolve => {
+      const idToFind = id;
+      const content = messages.filter(({ id }) => id == idToFind);
+      const size = content.length;
+
+      resolve({ size, content });
+    });
+  }
+
+  async remove(message) {
+    try {
+      return new Promise(async resolve => {
+        const content = await this.find(message);
+
+        const newData = messages.filter(({ text }) => text != message);
+        await saveData(newData);
+
+        resolve(content);
+      });
+    } catch (error) {
+      console.error('Error while removing message:', error);
+      throw new Error('Failed to remove message.');
+    }
+  }
+
+  async removeById(id) {
+    try {
+      return new Promise(async resolve => {
+        const idToRemove = id;
+        const content = await this.findById(idToRemove);
+
+        const newData = messages.filter(({ id }) => id != idToRemove);
+        await saveData(newData);
+
+        resolve(content);
+      });
+    } catch (error) {
+      console.error('Error while removing message:', error);
+      throw new Error('Failed to remove message.');
+    }
+  }
+}
+
+async function saveData(messagesArray) {
+  const filePath = 'data/messages.js';
+  
+  const messagesString = JSON.stringify(messagesArray, null, 2);
+  const fileContent = `export const messages = ${messagesString};`;
+
+  return new Promise((resolve, reject) => {
+    writeFile(filePath, fileContent, 'utf8', (error) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(`Messages data updated. Directory: ${filePath}`);
+      }
+    });
+  });
+}
+
+export default new MessageRepository();
