3e421747e8e603563696e513eaa72620fc8167c1
Renan <orefusado@gmail.com>
2023-06-27 17:24:39 -0300

    UpdateMessage service small improvements

--- app/services/UpdateMessage.js
+++ app/services/UpdateMessage.js
@@ -2,23 +2,22 @@ import MessageRepository from '../repositories/MessageRepository.js';
 
 export default class UpdateMessage {
   async execute(id, content, keepOldContent = true) {
-    if (id == null || (typeof id != 'number' && typeof id != 'string'))
+    if (id == null || !(typeof id === 'number' || typeof id === 'string'))
       throw new Error('Invalid ID.');
 
-    if (typeof content === 'object') {
-      if (content.hasOwnProperty('id')) {
-        delete content.id
-        console.log('Property "id" on the content was ignored. You can\'t edit the ID.');
-      }
-    } else {
+    if (typeof content !== 'object' || Array.isArray(content))
       throw new Error('Pass a new content as an object, others formats are invalid.');
+
+    if (content.hasOwnProperty('id')) {
+      delete content.id;
+      console.log('Property "id" on the content was ignored. You can\'t edit the ID.');
     }
 
     const message = await MessageRepository.findById(id);
 
-    if (!message.size) {
+    if (message.size <= 0) {
       console.log(`Message not found in ID "${id}".`);
-      return false;
+      return null;
     }
 
     if (keepOldContent) {
